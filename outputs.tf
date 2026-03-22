df388083479ef8b9d2813768b2be053cbd7d6af6
Cristian Beraha <cristian@cloudops.works>
2026-03-22 13:59:14 -0300

    chore: Update copyright year to 2026 and enhance output variables in Terraform files

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,43 @@
 ##
-# (c) 2021-2025
+# (c) 2021-2026
 #     Cloud Ops Works LLC - https://cloudops.works/
 #     Find us on:
 #       GitHub: https://github.com/cloudopsworks
 #       WebSite: https://cloudops.works
 #     Distributed Under Apache v2.0 License
 #
+
+output "network_container_id" {
+  value = mongodbatlas_network_container.this.container_id
+}
+
+output "network_container_provisioned" {
+  value = mongodbatlas_network_container.this.provisioned
+}
+
+output "network_container_aws_region" {
+  value = mongodbatlas_network_container.this.region_name
+}
+output "network_container_aws_vpc_id" {
+  value = mongodbatlas_network_container.this.vpc_id
+}
+
+output "network_container_gcp_project_id" {
+  value = mongodbatlas_network_container.this.gcp_project_id
+}
+
+output "network_container_gcp_network_name" {
+  value = mongodbatlas_network_container.this.network_name
+}
+
+output "network_container_azure_region" {
+  value = mongodbatlas_network_container.this.region
+}
+
+output "network_container_azure_subscription_id" {
+  value = mongodbatlas_network_container.this.azure_subscription_id
+}
+
+output "network_container_azure_vnet_name" {
+  value = mongodbatlas_network_container.this.vnet_name
+}
